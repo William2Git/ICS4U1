db02d81f9a74ea23ee758d42328e7a60cc5ae204
William Weng <william.weng@student.tdsb.on.ca>
2024-09-12 11:40:21 -0400

    Added confetti

---
+++ confetti.java
@@ -0,0 +1,4 @@
+public static void main(String[] args){
+    System.out.println("WELCOME TO OUR WORLD");
+    System.out.println("Catch as much confetti as you can, this is the ONLY time you will celebrate");
+}
