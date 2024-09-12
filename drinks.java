a773ddbe26e59ce289ab91481607958ed0488758
William Weng <william.weng@student.tdsb.on.ca>
2024-09-12 11:38:03 -0400

    Modified drinks

---
+++ drinks.java
@@ -0,0 +1,8 @@
+public static void main(String[] args){
+    System.out.println("You are not allowed to drink water");
+    System.out.println("Alcohol is no longer free");
+    System.out.println("A ticket is required for access. \nPlease acquire a ticket");
+    System.out.println("Good luck.");
+}
+
+
